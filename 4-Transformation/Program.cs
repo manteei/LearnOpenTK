170b837a8187bc2f79f48a6655adbef99472a4d5
Gromilova Maria <95652195+manteei@users.noreply.github.com>
2023-10-24 17:01:27 +0300

    Add files via upload

---
+++ 4-Transformation/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace LearnOpenTK
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game game = new Game(800, 600, "LearnOpenTK"))
+            {
+                game.Run();
+            }
+        }
+    }
+}
